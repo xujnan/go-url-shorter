6289ec75107191f43040a79091dd30fd8d492ac1
xujnan <1404152674@qq.com>
2023-12-06 01:07:45 +0800

    first

--- main.go
+++ main.go
@@ -1,5 +1,20 @@
-package go_url_shorter
+package main
+
+import (
+	"fmt"
+	"github.com/gin-gonic/gin"
+)
 
 func main() {
+	r := gin.Default()
+	r.GET("/", func(c *gin.Context) {
+		c.JSON(200, gin.H{
+			"message": "go-url-shorter",
+		})
+	})
 
+	err := r.Run(":9080")
+	if err != nil {
+		panic(fmt.Sprintf("failure to start web server, %s", err))
+	}
 }
